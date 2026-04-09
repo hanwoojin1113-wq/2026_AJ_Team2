fd2471cb6a1cc566adcf1b0d57e962eedff09217
hyeonseok <jack6692@naver.com>
2026-04-09 14:36:02 +0900

    마이페이지/상세검색

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "USER" (login_id, login_pw, nickname, gender, age) VALUES ('movieadmin', '0000', 'A137', 'MALE', 30);
+INSERT INTO "USER" (login_id, login_pw, nickname, gender, age) VALUES ('viewer01', '1234', 'B284', 'FEMALE', 24);
